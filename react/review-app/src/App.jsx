162c9ec7b00af9b89de22b913089aadab8d02d1d
jaganjavid <jagantutor1@gmail.com>
2024-12-05 19:04:13 +0530

    Added a react

---
+++ react/review-app/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header"
+
+const App = () => {
+
+
+  return (
+
+    <>
+    
+    <Header/>
+    <Header/>
+    <Header/>
+
+    <div className="container">
+      <h1>App</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
